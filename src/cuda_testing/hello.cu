3e2a17d915fb3324da0d0b219d8f65eefcdb777e
danielhufnagle <danielchufnagle@gmail.com>
2025-05-07 09:13:08 -0500

    added src folder and example CUDA script

---
+++ src/cuda_testing/hello.cu
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+// CUDA kernel function that runs on the GPU
+__global__ void hello() {
+  printf("Hello from thread %d in block %d\n", threadIdx.x, blockIdx.x);
+}
+
+int main() {
+  // print from CPU
+  std::cout << "Hello from CPU" << std::endl;
+  
+  // launch GPU kernel
+  // <<<num blocks, threads per block>>>
+  hello<<<2, 2>>>();
+
+  // wait for GPU to finish
+  cudaDeviceSynchronize();
+
+  return 0;
+}
